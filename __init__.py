25dc0e945bccc01939a12f85b6ee2e49291806ab
Joseph Allcock <joseph.allcock@durham.ac.uk>
2019-01-27 23:06:09 +0000

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from .cherab_image_mastu import *
-from .solps_profile import *
-from .tools import *
-from geo_matrix import *
-from invert_sart import *
-
-save_img_path = '/home/jallcock/cherab/pycis_cherab/saved_cherab_outputs/'
-save_calib_path = '/home/jallcock/cherab/pycis_cherab/saved_calcam_calibs/'
